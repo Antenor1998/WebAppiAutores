0367cc0687e2bd42dc705944baf4a0d09a2a6626
antenor <alopez@3code.us>
2023-02-28 17:31:07 -0600

    first commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Autor> Autores { get; set; }
+	}
+}
---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			
+			services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			// Configure the HTTP request pipeline.
+			if (env.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseRouting();
+
+			app.UseAuthorization();
+			
+			app.UseEndpoints( endpoints => 
+			{
+				endpoints.MapControllers();
+			});
+
+		}
+
+	}
+}
